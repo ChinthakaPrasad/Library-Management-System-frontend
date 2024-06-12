886b5bbcfefd3381105f6b1634a6b7043a88baa2
Chinthaka Prasad <chinthapc@gmail.com>
2024-06-12 12:45:44 +0530

    delete function implement

--- library-management-system/src/app/app.module.ts
+++ library-management-system/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './common/nav/nav.component';
 import { ViewAllBooksComponent } from './pages/view-all-books/view-all-books.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { ViewAllBooksComponent } from './pages/view-all-books/view-all-books.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    CommonModule
   ],
   providers: [
     provideClientHydration()
--- library-management-system/src/app/pages/view-all-books/view-all-books.component.ts
+++ library-management-system/src/app/pages/view-all-books/view-all-books.component.ts
@@ -1,5 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
@@ -9,15 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewAllBooksComponent implements OnInit{
   private http;
+  public bookList:any = {}; 
 
   constructor(private httpClient:HttpClient){
     this.http = httpClient;
   }
   ngOnInit(): void {
     this.loadAllBook();
+    console.log("Hello world");
   }
 
   loadAllBook():void{
-    this.http.get('')
+    this.http.get('http://localhost:8080/book/getBooks').subscribe((data)=>{
+      this.bookList = data;
+      console.log(this.bookList);
+    });
+  }
+
+  public selectBook:any;
+
+  deleteBook(){
+    this.http.delete(`http://localhost:8080/book/${this.selectBook.id}`).subscribe(data=>{
+      console.log(data);
+      this.loadAllBook();
+    });
+    
+  }
+
+  selectedBook(book:any){
+    console.log(book.id);
+    this.selectBook = book;
   }
 }
