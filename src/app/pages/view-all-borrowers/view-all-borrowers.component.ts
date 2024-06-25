692e72736f0b22a8bf072dfd8fde3668b745be9a
Chinthaka Prasad <chinthapc@gmail.com>
2024-06-26 00:00:37 +0530

    borrower front end create

--- src/app/pages/view-all-borrowers/view-all-borrowers.component.ts
+++ src/app/pages/view-all-borrowers/view-all-borrowers.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-view-all-borrowers',
   templateUrl: './view-all-borrowers.component.html',
   styleUrl: './view-all-borrowers.component.css'
 })
-export class ViewAllBorrowersComponent {
+export class ViewAllBorrowersComponent implements OnInit{
+
+  private http;
+  public borrowerList:any = {};
+
+  constructor(private httpClient:HttpClient){
+    this.http = httpClient;
+  }
+  ngOnInit(): void {
+
+    this.http.get("http://localhost:8081/user/getAllUser").subscribe(data =>{
+      this.borrowerList = data;
+      console.log(data);
+    })
+  }
+
+  public selectBorrower:any = {};
+
+  selectedBorrower(borrower:any){
+    this.selectBorrower = borrower;
+  }
+
+  deleteBorrower(){
+    this.http.delete(`http://localhost:8081/user/deleteUser/${this.selectBorrower.studentId}`).subscribe(data =>{
+      console.log(data);
+    })
+  }
+
+  loadBorrowers():void{
+    this.http.get("http://localhost:8081/user/getAllUser").subscribe(data =>{
+      this.borrowerList = data;
+    })
+  }
+
+  updateBorrower(){
+    this.http.post("http://localhost:8081/user/updateUser", this.selectBorrower).subscribe(data=>{
+      console.log(data);
+    })
+  }
+
+
+
+
+  
 
 }
